0a0f3f2270d0f436c9346d77a9d9604b457f8d01
yusuke <sato11.jp@gmail.com>
2021-10-28 02:03:09 +0900

    finished 3-3

--- memo/action/add.php
+++ memo/action/add.php
@@ -3,8 +3,8 @@
     require '../../common/database.php';
 
     if (!isLogin()) {
-        header('Location: ../login/');
-        exit;
+       header('Location: ../login/');
+       exit;
     }
 
     $user_id = getLoginUserId();
@@ -12,23 +12,20 @@
 
     try {
         $title = "新規メモ";
-
-        if ($statement = $database_handler->prepare("INSERT INTO memos (user_id, title, content) VALUES(:user_id, :title, null)"))
-        {
+        if ($statement = $database_handler->prepare("INSERT INTO memos (user_id, title, content) VALUES(:user_id, :title, null)")) {
             $statement->bindParam(":user_id", $user_id);
             $statement->bindParam(":title", $title);
             $statement->execute();
         }
 
         $_SESSION['select_memo'] = [
-            'id'      => $database_handler->lastInsertId(),
-            'title'   => $title,
+            'id' => $database_handler->lastInsertId(),
+            'title' => $title,
             'content' => '',
         ];
     } catch (Throwable $e) {
         echo $e->getMessage();
         exit;
     }
-
     header('Location: ../../memo');
     exit;
